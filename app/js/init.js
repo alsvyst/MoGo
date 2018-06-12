9545de74f3089b2fba852a0396fcd813bc374d7a
Alexandra Svystunova <sanyadiska@gmail.com>
2018-06-12 13:04:27 +0300

    quote and reviews

--- app/js/init.js
+++ app/js/init.js
@@ -2,4 +2,18 @@ $( function() {
     $( "#accordion" ).accordion({
         collapsible: true
     });
+    $('.quote-carousel').slick({
+        appendArrows: $('.quote-arrows'),
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>',
+        infinite: true,
+        speed: 300,
+        fade: true,
+        cssEase: 'linear'
+    });
+    $('.reviews-carousel').slick({
+        appendArrows: $('.reviews-arrows'),
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>',
+    });
 } );
